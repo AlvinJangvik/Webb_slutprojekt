fcc214a547c155f91b491329dc935d16ef802bbb
AlvinJangvik <54435532+AlvinJangvik@users.noreply.github.com>
2021-04-06 14:30:56 +0200

    Start
    Index, footer, css, icon

---
+++ Alvin_Slutprojekt/Html/index.php
@@ -0,0 +1,5 @@
+<?php
+	session_start();
+	
+	require "../Templates/index_template.php";
+?>
---
+++ Alvin_Slutprojekt/Templates/index_template.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="sv">
+	<head>
+		<meta charset="utf-8">
+		<title>Avvelitos mat blogg</title>
+		<link rel="stylesheet" href="css/stilmall.css">
+		<link rel="icon" type="image/png" href="bilder/favicon-32x32.png"/>
+	</head>
+	<body>
+		<?php
+			require "footer.php";
+		?>
+	</body>
+</html>
